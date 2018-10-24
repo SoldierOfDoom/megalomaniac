125b719ddc27c422b9d2d7d6a62d9a14a6597580
SoldierOfDoom <44431155+SoldierOfDoom@users.noreply.github.com>
2018-10-24 16:10:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,56 +4,8 @@ const config = require('./config.json');
 
 const client = new Discord.Client();
 
-const { Client, RichEmbed } = require('discord.js');
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', message => {
-  if (message.content === 'Embed') {
-    const embed = new RichEmbed()
-    .setTitle('Приключения Арсения')
-    .setColor(0xFF0000)
-    .setDescription('Часть 2')
-    .setThumbnail("https://media.discordapp.net/attachments/491209089360068611/491934166242557953/afsasfafs.jpg");
-    message.delete()
-    message.channel.send(embed);
-  }
-});
-
-client.on('message', message => {
-  if (message.content === 'Embed1') {
-    const embed = new RichEmbed()
-    .setTitle("This is your title, it can hold 256 characters")
-  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
-  /*
-   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-   */
-  .setColor(0x00AE86)
-  .setDescription("This is the main body of text, it can hold 2048 characters.")
-  .setFooter("ёпта", "http://i.imgur.com/w1vhFSR.png")
-  .setImage("https://images2.domashnyochag.ru/upload/img_cache/cfd/cfd9dd3c5f2f3eb57384d836e0cf7a65_ce_1000x555x0x97_cropped_930x510_fitted_1260x700.jpg")
-  .setThumbnail("https://st.kp.yandex.net/images/actor_iphone/iphone360_3098539.jpg")
-  /*
-   * Takes a Date object, defaults to current date.
-   */
-  .setTimestamp()
-  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
-  .addField("Азазазазаз нахуй",
-    "Сукабля")
-  /*
-   * Inline fields may not display as inline if the thumbnail and/or image is too big.
-   */
-  .addField("Inline Field", "They can also be inline.", true)
-  /*
-   * Blank field, useful to create some space.
-   */
-  .addBlankField(true)
-  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
-  message.delete()
-    message.channel.send(embed);
-  }
 });
 
 client.on("message", (message) => {
